a1f14e518e609106ede31b9cace214994bd34572
Carlos Valdivia <carlord5272009@gmail.com>
2022-05-18 00:44:36 -0500

    limpiando archivos

--- DooFast/Global.asax.cs
+++ DooFast/Global.asax.cs
@@ -17,7 +17,7 @@ namespace DooFast
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        
         }
     }
 }
