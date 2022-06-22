cabf0a754ade5ed6c1acf7f29147f206a8e8dd1b
Carlos Valdivia <carlord5272009@gmail.com>
2022-06-22 16:11:46 -0500

    Agregando pruebas unitarias POST - LIST a ComidasController

---
+++ UnitTestDooFast/Controllers/Comidas.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DooFast.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using CapaEntidades;
+
+namespace DooFast.Controllers.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+      
+        public void GetComidaTest()
+        {
+            ComidasController comidas = new ComidasController();
+
+            var result = comidas.Get();
+
+            //test respuesta del servidor
+
+            Assert.AreNotSame(comidas, System.Net.HttpStatusCode.NotFound);
+
+
+            //test listado
+            Assert.AreNotSame(null, result);
+            Assert.AreEqual(15, result.Count());
+        }
+
+        [TestMethod()]
+        //[UrlToTest("https://localhost:44301/")
+        public void PostComdiaTest() {
+
+
+            ComidasController comidas = new ComidasController();
+
+            ComidaBE comidaPrueba = new ComidaBE();
+
+            comidaPrueba.idCategoria = 1;
+            comidaPrueba.nombreComida = "Ensalada de cebolla";
+            comidaPrueba.costo = 20;
+            comidaPrueba.precio = 10;
+
+            var result = comidas.Post(comidaPrueba);
+
+            List<ComidaBEforList> lstComidasVacia = new List<ComidaBEforList>();
+            
+            Assert.AreNotSame(null, result);
+            Assert.AreNotSame(lstComidasVacia, result);
+
+        }
+    }
+}
